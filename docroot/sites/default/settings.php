c1732637512ba856e37de07c7d0372f65c9c4c57
Jose Moreira <jose.moreira@acquia.com>
2018-02-04 16:11:56 -0500

    DEMO-005: Switching default site to Lightning.

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -784,4 +784,4 @@ $settings['entity_update_batch_size'] = 50;
 #   include $app_root . '/' . $site_path . '/settings.local.php';
 # }
 require DRUPAL_ROOT . "/../vendor/acquia/blt/settings/blt.settings.php";
-$settings['install_profile'] = 'standard';
+$settings['install_profile'] = 'lightning';
